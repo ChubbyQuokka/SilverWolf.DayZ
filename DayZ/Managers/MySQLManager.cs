6fc2ca36078dbb1587ac65e855c41df0abab6676
ChubbyQuokka <graybad1@icloud.com>
2018-03-22 13:25:45 -0400

    .

--- DayZ/Managers/MySQLManager.cs
+++ DayZ/Managers/MySQLManager.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using ChubbyQuokka.DayZ.Structures;
+
 namespace ChubbyQuokka.DayZ.Managers
 {
     internal static class MySQLManager
     {
+        static DatabaseSettings Settings => DayZConfiguration.DatabaseSettings;
 
+        static class Queries
+        {
+            
+        }
     }
 }
